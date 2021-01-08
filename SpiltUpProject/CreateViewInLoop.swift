7de210ba707bcaa533f795a99b2eed3dbdbb7ade
Hung-Chun, Tsai <tsaihong1995@gmail.com>
2021-01-08 12:47:12 -0500

    Add .gitignore file

---
+++ SpiltUpProject/CreateViewInLoop.swift
@@ -0,0 +1,20 @@
+//
+//  CreateViewInLoop.swift
+//  SpiltUpProject
+//
+//  Created by Hung-Chun Tsai on 2021-01-08.
+//
+
+import SwiftUI
+
+struct CreateViewInLoop: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateViewInLoop_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateViewInLoop()
+    }
+}
