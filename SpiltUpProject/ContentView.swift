ba9b8afc09bf14720bbecfc2a948672947d06a77
Hung-Chun, Tsai <tsaihong1995@gmail.com>
2021-01-08 12:14:00 -0500

    Initial Commit

---
+++ SpiltUpProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpiltUpProject
+//
+//  Created by Hung-Chun Tsai on 2021-01-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpiltUpProject/SpiltUpProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiltUpProjectApp.swift
+//  SpiltUpProject
+//
+//  Created by Hung-Chun Tsai on 2021-01-08.
+//
+
+import SwiftUI
+
+@main
+struct SpiltUpProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
